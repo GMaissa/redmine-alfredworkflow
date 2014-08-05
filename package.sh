5b61abe9969b3c9de30d74c9d9307edeac851488
Guillaume Maïssa <guillaume@maissa.fr>
2014-08-06 00:01:41 +0200

    adding a packaging script

---
+++ package.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -x
+
+rm Redmine.alfredworkflow
+composer update --no-dev
+zip -r Redmine . -x config/settings.json -x package.sh \*phpunit.xml.dist \*composer.json \*composer.lock \*.gitignore \*.travis.yml \*.gitkeep \
+                 -x \*test/\* \*.idea/\* \*coverage/\* \*.git/\* \*Tests/\*
+mv Redmine.zip Redmine.alfredworkflow
