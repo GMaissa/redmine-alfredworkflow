da30a28d5a774286d43d7fde03ef305c7574edc6
Guillaume Maïssa <guillaume@maissa.fr>
2015-02-27 21:45:28 +0100

    fix a checkstyle issue

--- src/AlfredWorkflow/Redmine/Actions/PageAction.php
+++ src/AlfredWorkflow/Redmine/Actions/PageAction.php
@@ -403,6 +403,8 @@ class PageAction extends BaseAction
      * Retrieve data (project details) from Redmine server
      *
      * @param string $redmineId redmine server identifier
+     *
+     * @return void
      */
     protected function loadRedmineData($redmineId)
     {
